97b2c866a48fc8a1d64d5fb6fa0ac46732b322db
BoredBoi <68980580+BoredSathvik@users.noreply.github.com>
2021-10-16 07:30:18 +0530

    Removed Extra code, and nested dirs
    idk why you guys did that but ok

--- BuildModules.bat
+++ BuildModules.bat
@@ -1,10 +1,7 @@
 ::Setup submodules
-cd Nuvola
-cd Lib
-cd PolyHook_2_0
+cd Nuvola/Lib/PolyHook_2_0
+
 :: Build polyhook
 cmake -B"./_build" -DCMAKE_INSTALL_PREFIX="./_install/" -DPOLYHOOK_BUILD_SHARED_LIB=OFF
 cmake --build "./_build" --config Release --target INSTALL
-cd ..
-cd ..
-cd ..
+cd ../../../
