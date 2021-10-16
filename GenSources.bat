82516540e8483f28e2b95d7d3950352bdadd81e8
BoredBoi <68980580+BoredSathvik@users.noreply.github.com>
2021-10-16 07:31:02 +0530

    Updated GenSources

--- GenSources.bat
+++ GenSources.bat
@@ -4,5 +4,4 @@ cmake .
 cmake --build . --config Release
 cd Release
 ClassBuilder.exe --proj "../../Nuvola/Client/Bridge/bridge.json" --gen CppGen --output "../../Nuvola/Client/Bridge/"
-cd ..
-cd ..
+cd ../../
